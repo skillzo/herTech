ce4c772b3c0e76457e70a82632a3bb0018f2f0ee
skillzo <skillzfire@yahoo.com>
2022-08-10 21:26:07 +0100

    fixed the navbar

--- src/components/navBar.js
+++ src/components/navBar.js
@@ -1,8 +1,8 @@
 import React, { useState } from "react";
 import "./navBar.css";
-import GitHubIcon from '@mui/icons-material/GitHub';
-import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+import GitHubIcon from "@mui/icons-material/GitHub";
+import MenuIcon from "@mui/icons-material/Menu";
+import CloseIcon from "@mui/icons-material/Close";
 function NavBar() {
   const [isClicked, setIsClicked] = useState(true);
 
@@ -21,18 +21,25 @@ function NavBar() {
           Her<span>Tech</span>
         </h1>
       </div>
-      <div style={{ display: isClicked ? "none" :"block" }} className="navbar">
+      <div style={{ display: isClicked ? "none" : "block" }} className="navbar">
         <nav>
-          <a href="#"> Job Listing</a>
-          <a href="#"> Find a Mentor</a>
-          <a href="#"> Find a Community</a>
-          <a href="https://github.com/skillzo"> <GitHubIcon />My Github</a>
-          <a href="#"> more</a>
+          <a href="/"> Job Listing</a>
+          <a href="/"> Find a Mentor</a>
+          <a href="/"> Find a Community</a>
+          <a href="https://github.com/skillzo">
+            {" "}
+            <GitHubIcon />
+            My Github
+          </a>
+          <a href="/"> more</a>
         </nav>
         <button className="button1 post-a-job">Post a Job </button>
       </div>
       <div onClick={clickHandler} className="menu-btn">
-        <div className="menu-btn-burger"> {isClicked? <MenuIcon/> : <CloseIcon /> }  </div>
+        <div className="menu-btn-burger">
+          {" "}
+          {isClicked ? <MenuIcon /> : <CloseIcon />}{" "}
+        </div>
       </div>
     </div>
   );
